cda74735e7c3b31a2bdbdd8fbeda9d3a0f3e5bec
Adam Dahlquist <adahlquist@xmatters.com>
2017-06-08 15:41:31 -0700

    d

--- src/main/java/com/battlesnake/strategy/CheckSnakeTail.java
+++ src/main/java/com/battlesnake/strategy/CheckSnakeTail.java
@@ -9,8 +9,8 @@ import java.util.List;
 
 public class CheckSnakeTail implements BasicStrategy {
 
-    @Override public List<Move> makeAMove(MoveRequest request, List<Move> possibleMoves) {
-         Snake me = SnakeUtils.findSnakeByUUID(request.getYou(),request.getSnakes());
+    @Override public List<Move> makeAMove(MoveRequest request, List<Move> moves) {
+        Snake me = SnakeUtils.findSnakeByUUID(request.getYou(),request.getSnakes());
 
         List<Move> remainingMoves = new ArrayList<>();
         if(me != null) {
@@ -21,25 +21,29 @@ public class CheckSnakeTail implements BasicStrategy {
             int[] leftCoords = SnakeUtils.getPossibleMoveCoord(head, Move.LEFT);
             int[] rightCoords = SnakeUtils.getPossibleMoveCoord(head, Move.RIGHT);
 
+            List<Move> possibleMoves = new ArrayList<>();
+            possibleMoves.add(Move.UP);
+            possibleMoves.add(Move.DOWN);
+            possibleMoves.add(Move.RIGHT);
+            possibleMoves.add(Move.LEFT);
+
             for(int i=1; i<coords.length; i++) {
-                if (!SnakeUtils.sameCoords(upCoords, coords[i])) {
-                    remainingMoves.add(Move.UP);
+                if (SnakeUtils.sameCoords(upCoords, coords[i])) {
+                    possibleMoves.remove(Move.UP);
                 }
 
                 if (!SnakeUtils.sameCoords(downCoords, coords[i])) {
-                    remainingMoves.add(Move.DOWN);
+                    possibleMoves.remove(Move.DOWN);
                 }
 
                 if (!SnakeUtils.sameCoords(leftCoords, coords[i])) {
-                    remainingMoves.add(Move.LEFT);
+                    possibleMoves.remove(Move.LEFT);
                 }
 
                 if (!SnakeUtils.sameCoords(rightCoords, coords[i])) {
-                    remainingMoves.add(Move.RIGHT);
+                    possibleMoves.remove(Move.RIGHT);
                 }
             }
-
-
         }
         return remainingMoves;
     }
