a5ddf724dc7d5d8913b2006f58c3fdd09bc901a9
Adam Dahlquist <adahlquist@xmatters.com>
2017-06-08 14:56:57 -0700

    edge of board

--- src/main/java/com/battlesnake/strategy/CheckEdgeOfBoard.java
+++ src/main/java/com/battlesnake/strategy/CheckEdgeOfBoard.java
@@ -11,15 +11,21 @@ import java.util.List;
  * Created by adahlquist on 2017-06-08.
  */
 public class CheckEdgeOfBoard implements BasicStrategy {
-    List<Move> makeAMove(MoveRequest request, List<Move> possibleMoves) {
+    public List<Move> makeAMove(MoveRequest request, List<Move> possibleMoves) {
+        List<Move> nextPossibleMoves = new ArrayList<>();
+
         Snake us = SnakeUtils.findSnakeByUUID(request.getYou(), request.getSnakes());
         int boardSize = request.getHeight();
 
         for (Move move: possibleMoves) {
-            if (SnakeUtils.getPossibleMoveCoord(us.getCoords()[0], possibleMoves);
+            int[] possbileMoveCoord = SnakeUtils.getPossibleMoveCoord(us.getCoords()[0], move);
+            if (!(possbileMoveCoord[0] < 0 || possbileMoveCoord[0] == boardSize ||
+                    possbileMoveCoord[1] < 0 || possbileMoveCoord[1] == boardSize)) {
+                nextPossibleMoves.add(move);
+            }
         }
 
-        return new ArrayList<Move>();
+        return nextPossibleMoves;
     }
 
 
